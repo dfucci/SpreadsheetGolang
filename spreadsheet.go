f459162adcb34f95518b7dd0902e7aaffceec802
dfucci <davfuc@gmail.com>
2016-11-03 12:22:47 +0200

    Fix warning

--- spreadsheet.go
+++ spreadsheet.go
@@ -2,36 +2,41 @@ package main
 
 import "fmt"
 
+// Spreadsheet general type
 type Spreadsheet struct {
-  cells map[string]int
+	cells map[string]int
 }
 
-func NewSpreadsheet() *Spreadsheet{
-  spreadsheet := new(Spreadsheet)
-  spreadsheet.cells = make(map[string]int)
-  return spreadsheet
+// NewSpreadsheet creates a spreadsheet type
+func NewSpreadsheet() *Spreadsheet {
+	spreadsheet := new(Spreadsheet)
+	spreadsheet.cells = make(map[string]int)
+	return spreadsheet
 }
 
 type err interface {
-  Error() string
-}
-type Methods interface {
-  Set(cell string, value int)
-  Get(cell string) int
+	Error() string
 }
 
+type methods interface {
+	Set(cell string, value int)
+	Get(cell string) int
+}
+
+// Set a cell of the spreadsheet
 func (s *Spreadsheet) Set(cell string, value int) {
-  s.cells[cell] = value
+	s.cells[cell] = value
 }
 
+// Get a cell of a spreadsheet
 func (s *Spreadsheet) Get(cell string) int {
-  elem := s.cells[cell]
-  return elem
+	elem := s.cells[cell]
+	return elem
 }
 
 // Using this for debugging purpose
-func main(){
-  s := NewSpreadsheet()
-  s.Set("A1", 8)
-  fmt.Printf("%d", s.Get("A1"))
+func main() {
+	s := NewSpreadsheet()
+	s.Set("A1", 8)
+	fmt.Printf("%d", s.Get("A1"))
 }
--- spreadsheet_test.go
+++ spreadsheet_test.go
@@ -1,13 +1,13 @@
 package main
 
 import "testing"
-func TestSingleCell(t *testing.T){
-  s := NewSpreadsheet()
-  s.Set("A1", 2)
-  actual := s.Get("A1")
-  expected := 2
-  if actual != expected {
-    t.Errorf("Test failed, expected: %d, got %d", expected, actual)
-  }
-}
 
+func TestSingleCell(t *testing.T) {
+	s := NewSpreadsheet()
+	s.Set("A1", 2)
+	actual := s.Get("A1")
+	expected := 2
+	if actual != expected {
+		t.Errorf("Test failed, expected: %d, got %d", expected, actual)
+	}
+}
